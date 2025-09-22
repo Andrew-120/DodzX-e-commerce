21374833029d7fc3d084bf213e7693c2d8ab56c4
Andrew Melad Makram <andrew.melad1@msa.edu.eg>
2025-09-23 02:03:03 +0300

    Add to cart production

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -2,10 +2,12 @@
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
-
-
 export async function getDecodedToken() {
-  const encodedToken = (await cookies()).get("next-auth.session-token")?.value;
+  const cookieStore = await cookies();
+  const encodedToken =
+    cookieStore.get("next-auth.session-token")?.value ||
+    cookieStore.get("__Secure-next-auth.session-token")?.value;
+
   const decodedToken = await decode({
     token: encodedToken,
     secret: process.env.AUTH_SECRET!,
@@ -15,12 +17,9 @@ export async function getDecodedToken() {
 }
 
 export async function getUserToken() {
-  
   return (await getDecodedToken())?.token;
 }
 
-
 export async function getUserId() {
- 
   return (await getDecodedToken())?.sub;
 }
